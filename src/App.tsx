17335534b5b220fdd03785b826f8fa961efea4d2
devofficer <myeyesontech@gmail.com>
2022-11-17 13:16:20 -0800

    [feat] simple ui for input and submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,59 @@
-import * as React from "react"
+import React, { useState } from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
   Code,
   Grid,
   theme,
+  Stack,
+  VStack,
+  HStack,
+  Input,
+  Button,
+  Card, 
+  Heading,
+  StackDivider,
+  CardHeader, 
+  CardBody, 
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="10vmin" pointerEvents="none" />
-          <Text>
-            Please submit stock ticker symbol to fetch information.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  const [ticker, setTicker] = React.useState<string>('');
+  const handleSubmit = () => {
+    alert(ticker);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTicker(e.target.value);
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack direction="column" spacing={2}>
+            <Card>
+              <CardBody>
+                <HStack alignItems="center" mb={5}>
+                  <Logo h="5vmin" pointerEvents="none" />
+                  <Heading size='md'>Stock App</Heading>
+                </HStack>
+
+                <Stack divider={<StackDivider />} spacing='2'>
+                  <HStack>
+                    <Input placeholder='e.g AAPL, TSLA, AMZN' type='text' value={ ticker } onChange={ handleChange } />
+                    <Button onClick={ handleSubmit }>Submit</Button>
+                  </HStack>
+                  
+                </Stack>
+              </CardBody>
+            </Card>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
